943294541ee075af785045ac4a4802bacba4dfc2
Mythily <mythily.mudunuru@gmail.com>
2019-04-11 03:11:58 +0000

    updated with loops

--- areaCalculators.js
+++ areaCalculators.js
@@ -3,21 +3,24 @@
 function calculateRectangleArea(length, width){
   if (length < 1 || width < 1){
     return undefined;
-} else
-  return length * width;
+  } else {
+    return length * width;
+  }
 }
 
 function calculateTriangleArea(base, height){
   if ( base < 1 || height < 1){
     return undefined;
-}
-  return base * height / 2;
+  } else{
+    return base * height / 2;
+  }
 }
 function calculateCircleArea(radius){
   if (radius < 1){
     return undefined;
-}
-  return Math.PI * radius * radius;
+  } else{
+    return Math.PI * radius * radius;
+  }
 }
 
 console.log(calculateRectangleArea(10, 5));     // should print 50
