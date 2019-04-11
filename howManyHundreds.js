251826ab0046d07cc3761c8cb7c92ad2e2d7f6f5
Mythily <mythily.mudunuru@gmail.com>
2019-04-11 02:00:24 +0000

    uptp array exercises

---
+++ howManyHundreds.js
@@ -0,0 +1,6 @@
+function howManyHundreds(number){
+  return Math.trunc(number / 100);
+}
+
+console.log (howManyHundreds(10000));
+console.log (howManyHundreds(894));
---
+++ optionalArguments.js
@@ -0,0 +1,10 @@
+function power(base, exponent = 2) {
+  let result = 1;
+  for (let count = 0; count < exponent; count++) {
+    result *= base;
+  }
+  return result;
+}
+
+console.log(power(4));
+console.log(power(2, 6));
